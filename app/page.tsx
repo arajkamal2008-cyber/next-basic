c38c5f914bfb5fb1b2dd62bdb0c3c79f12a0b61f
arajkamal2008-cyber <arajkamal@2008gmail.com>
2026-06-12 18:06:05 +0530

    creatuser

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
+import CreatUser from "@/components/aboutComponents/homeComponents/CreatUser";
 import Loops from "@/components/aboutComponents/homeComponents/Loops";
 
 
 export default function Home() {
-  return (<>
+
+  return (<> <CreatUser/>
+  <hr className="my-20" />
+
   <Loops/>
   </>);
 }
